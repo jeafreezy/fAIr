010e39dfa69874a45034602d6449969b8a267815
jeafreezy <jolaiyaemmanuel@gmail.com>
2025-02-23 09:12:33 +0100

    chore: setting up lint-staged + husky

--- frontend/src/utils/cn.ts
+++ frontend/src/utils/cn.ts
@@ -4,7 +4,6 @@ import { twMerge } from "tailwind-merge";
 /**
  * Tailwind utility function for managing classnames.
  */
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
